1d1e4089b703776ba213519e4e49f542496726f5
britt-tech-services <95835957+britt-tech-services@users.noreply.github.com>
2024-11-27 11:42:14 -0600

    update script

--- Deployment/scripts/copy_kb_files.sh
+++ Deployment/scripts/copy_kb_files.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Installing Python 3.10 and pip"
-apt-get update && apt-get install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa -y
-apt-get update && apt-get install -y python3.10 python3.10-venv python3.10-distutils
+# echo "Installing Python 3.10 and pip"
+# apt-get update && apt-get install -y software-properties-common
+# add-apt-repository ppa:deadsnakes/ppa -y
+# apt-get update && apt-get install -y python3.10 python3.10-venv python3.10-distutils
 
 # # Set Python 3.10 as the default
 # ln -sf /usr/bin/python3.10 /usr/bin/python
 # ln -sf /usr/bin/python3.10 /usr/bin/python3
 
 # # Verify Python version
-# python --version
+echo "Python version:"
+python --version
 
 # # Install pip for Python 3.10
 # curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
@@ -22,8 +23,11 @@ python3 -m venv /tmp/venv
 
 # Activate the virtual environment
 source /tmp/venv/bin/activate
+
+echo "install lancedb"
+pip install lancedb==0.13.0
 # pip install graphrag==0.3.6
-pip install -r https://raw.githubusercontent.com/brittneek/graphrag-deployment/main/Deployment/scripts/graphrag-requirements.txt
+# pip install -r https://raw.githubusercontent.com/brittneek/graphrag-deployment/main/Deployment/scripts/graphrag-requirements.txt
 # python -m graphrag.index --root ./ --verbose
 # echo "open graphrag folder"
 # cd graphrag
